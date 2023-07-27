c77cd9619ceed57a2df0ef95a0bf2ae57b888bdf
Steven S <freelancers2010@gmail.com>
2023-07-26 18:25:34 -0600

    Updates

--- src/DataForm.js
+++ src/DataForm.js
@@ -3,45 +3,71 @@ import { collection, addDoc } from "firebase/firestore";
 import { getDocs } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 import DataList from "./DataList";
+import { Modal } from "flowbite";
 
 const DataForm = () => {
+  // set the modal menu element
+  const $targetEl = document.getElementById("formModal");
+
+  // options with default values
+  const options = {
+    // placement: "bottom-right",
+    backdrop: "dynamic",
+    // backdropClasses: "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40",
+    closable: true,
+    onHide: () => {
+      console.log("modal is hidden");
+    },
+    onShow: () => {
+      console.log("modal is shown");
+    },
+    onToggle: () => {
+      console.log("modal has been toggled");
+    },
+  };
   const [formData, setFormData] = useState({
     payment: "",
     amount: "",
+    currency: "",
     time: "",
     notes: "",
   });
   const [data, setData] = useState([]);
-  // const [payment, setPayment] = useState("");
-  // const [amount, setAmount] = useState("");
-  const [time, setTime] = useState("");
   const [disabled, setDisabled] = useState(true);
+  const [invalidPayment, setInvalidPayment] = useState(false);
+  const [invalidAmount, setInvalidAmount] = useState(false);
+  const [invalidCurrency, setInvalidCurrency] = useState(false);
+  const modal = new Modal($targetEl, options);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (formData.payment && formData.amount) {
-      console.log("Form submitted successfully!");
+    if (formData.payment && formData.amount && formData.currency) {
       try {
+        const time = new Date().toLocaleTimeString();
         const docRef = await addDoc(collection(db, "caja"), {
           payment: formData.payment,
           amount: formData.amount,
+          currency: formData.currency,
           time: time,
           notes: formData.notes,
         });
         console.log("Document written with ID: ", docRef.id);
-        setFormData({
-          payment: "",
-          amount: "",
-          time: "",
-          notes: "",
-        }); // Clear form
-        setDisabled(true);
+        getData();
+        hideModal();
       } catch (e) {
         console.error("Error adding document: ", e);
       }
     } else {
       console.log("Please fill out all fields.");
+      validateForm();
     }
+  };
+
+  const validateForm = () => {
+    console.log("validateForm...", formData.amount);
+    !formData.payment ? setInvalidPayment(true) : setInvalidPayment(false);
+    !formData.amount ? setInvalidAmount(true) : setInvalidAmount(false);
+    !formData.currency ? setInvalidCurrency(true) : setInvalidCurrency(false);
   };
 
   const handleChange = (e) => {
@@ -50,9 +76,7 @@ const DataForm = () => {
       ...prevFormData,
       [name]: value,
     }));
-    if (formData.payment && formData.amount) {
-      setDisabled(false);
-    }
+    validateForm();
   };
 
   const getData = async () => {
@@ -63,55 +87,117 @@ const DataForm = () => {
       console.log(`${data.id} => ${data.data()}`);
       array.push(data.data());
     });
-    console.log("array...", array);
     setData(array);
-    console.log("dbData...", data);
   };
 
   useEffect(() => {
+    console.log("useEffect...");
     getData();
-    setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 1000);
+    // setInterval(() => {
+    //   setTime(new Date().toLocaleTimeString());
+    // }, 1000);
   }, []);
+
+  const hideModal = () => {
+    modal.hide();
+    let btn = document.getElementById("formModal");
+    btn.click();
+  };
+
+  const showModal = () => {
+    modal.show();
+  };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="payment">
-            Tipo de pago:
-          </label>
-          <select id="payment" name="payment" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" value={formData.payment} onChange={handleChange}>
-            <option>Seleccione el tipo de pago</option>
-            <option value="tarjeta">Tarjeta</option>
-            <option value="efectivo">Efectivo</option>
-          </select>
+      <div className="flex items-center justify-center">
+        <button id="open-form-modal" data-modal-target="formModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" onClick={showModal}>
+          Agregar Orden
+        </button>
+      </div>
+      <div id="formModal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div className="relative w-full max-w-md max-h-full">
+          <div className="relative bg-white rounded-lg shadow">
+            <button id="close-form-modal" type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="formModal" onClick={hideModal}>
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="px-6 py-6 lg:px-8">
+              <h3 className="mb-4 text-xl font-medium text-gray-900">Registrar nueva orden</h3>
+              <form onSubmit={handleSubmit} className="space-y-6 mb-4">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="payment">
+                    Tipo de pago:
+                  </label>
+                  <select id="payment" name="payment" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" value={formData.payment} onChange={handleChange}>
+                    <option>Seleccione el tipo de pago</option>
+                    <option value="tarjeta">Tarjeta</option>
+                    <option value="efectivo">Efectivo</option>
+                  </select>
+                  {invalidPayment && (
+                    <div id="payment-error" className="px-2 py-2 mt-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 " role="alert">
+                      <span className="font-medium">*</span> Tipo de pago requerido!
+                    </div>
+                  )}
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
+                    Monto:
+                  </label>
+                  <input type="number" id="amount" name="amount" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Monto" value={formData.amount} onChange={handleChange} />
+                  {invalidAmount && (
+                    <div id="currency-error" className="px-2 py-2 mt-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 " role="alert">
+                      <span className="font-medium">*</span> Monto requerido!
+                    </div>
+                  )}
+                </div>
+                <div className="mb-4">
+                  <div className="flex gap-5">
+                    <div className="flex items-center">
+                      <input id="currency-1" type="radio" name="currency" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" value="colones" onClick={handleChange} />
+                      <label htmlFor="currency-1" className="ml-2 text-sm font-medium text-gray-900">
+                        Colones
+                      </label>
+                    </div>
+                    <div className="flex items-center">
+                      <input id="currency-2" type="radio" name="currency" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" value="dolares" onClick={handleChange} />
+                      <label htmlFor="currency-2" className="ml-2 text-sm font-medium text-gray-900">
+                        Dolares
+                      </label>
+                    </div>
+                  </div>
+                  {invalidCurrency && (
+                    <div id="currency-error" className="px-2 py-2 mt-2 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 " role="alert">
+                      <span className="font-medium">*</span> Moneda requerida!
+                    </div>
+                  )}
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="notes">
+                    Notas:
+                  </label>
+                  <textarea id="notes" name="notes" rows="4" className="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Alguna nota especial para esta orden..." value={formData.notes} onChange={handleChange}></textarea>
+                </div>
+                {/* <div className="mb-6">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="time">
+                    Hora: {time}
+                  </label>
+                  <input type="hidden" id="time" name="time" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Hora" value={time} />
+                </div> */}
+                isDisabled: {disabled}
+                <div className="flex justify-center">
+                  <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Salvar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="amount">
-            Monto:
-          </label>
-          <input type="text" id="amount" name="amount" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Monto" value={formData.amount} onChange={handleChange} />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="time">
-            Hora:
-          </label>
-          <input type="text" id="time" name="time" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Hora" value={time} disabled />
-        </div>
-        <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="notes">
-            Notas
-          </label>
-          <textarea id="notes" name="notes" rows="4" className="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring focus:ring-indigo-200" placeholder="Alguna nota en especial..." value={formData.notes} onChange={handleChange}></textarea>
-        </div>
-        <div className="flex justify-center">
-          <button type="submit" className="px-6 py-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-md focus:outline-none focus:ring focus:ring-indigo-200" disabled={disabled}>
-            Agregar
-          </button>
-        </div>
-      </form>
+      </div>
+
       <DataList data={data} />
     </div>
   );
--- src/DataList.js
+++ src/DataList.js
@@ -1,16 +1,108 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Modal } from "flowbite";
 
 const DataList = ({ data }) => {
+  const $targetEl = document.getElementById("detailsModal");
+  const options = {
+    // placement: "bottom-right",
+    backdrop: "dynamic",
+    // backdropClasses: "bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40",
+    closable: true,
+    onHide: () => {
+      console.log("modal is hidden");
+    },
+    onShow: () => {
+      console.log("modal is shown");
+    },
+    onToggle: () => {
+      console.log("modal has been toggled");
+    },
+  };
+  const modal = new Modal($targetEl, options);
+
+  useEffect(() => {});
+
+  const showModal = (e) => {
+    modal.show();
+  };
+
+  const hideModal = () => {
+    modal.hide();
+    let btn = document.getElementById("formModal");
+    btn.click();
+  };
   return (
-    <div>
-      <h2>Montos:</h2>
-      <ul>
-        {data.map((data, index) => (
-          <li key={index}>
-            Cantidad: {data.amount}, Tipo de pago: {data.payment} Hora: {data.time}
-          </li>
-        ))}
-      </ul>
+    <div className="p-4">
+      <div className="relative overflow-x-auto max-h-96">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Pago
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Cantidad
+              </th>
+              {/* <th scope="col" className="px-6 py-3">
+                Hora
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Notas
+              </th> */}
+              <th scope="col" className="px-6 py-3">
+                Accion
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((data, index) => (
+              <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <span className="capitalize">{data.payment}</span>
+                </th>
+                <td className="px-6 py-4">₡{Intl.NumberFormat("en-US").format(data.amount)}</td>
+                {/* <td className="px-6 py-4">{data.time}</td>
+                <td className="px-6 py-4">{data.notes ? data.notes : "N/A"}</td> */}
+                <td className="px-6 py-4">
+                  <button data-modal-target="detailsModal" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" onClick={showModal}>
+                    Editar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div id="detailsModal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div className="relative w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Terms of Service</h3>
+                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center" data-modal-hide="detailsModal" onClick={hideModal}>
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                  <span className="sr-only">Close modal</span>
+                </button>
+              </div>
+
+              <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.</p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.</p>
+              </div>
+
+              <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="detailsModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  I accept
+                </button>
+                <button data-modal-hide="detailsModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                  Decline
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
