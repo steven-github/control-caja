6b5be49942ff83124364e5579e2e345bbd47342b
Steven S <freelancers2010@gmail.com>
2023-07-27 13:38:52 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import DataForm from "./DataForm";
 
 function App() {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  const [screenHeight, setScreenHeight] = useState(window.screen.height);
+
+  // Event handler for updating the viewport width on resize
+  const handleResize = () => {
+    setViewportWidth(window.innerWidth);
+    setViewportHeight(window.innerHeight);
+    setScreenHeight(window.screen.height);
+  };
+
+  useEffect(() => {
+    // Add event listener when the component mounts
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener when the component unmounts to avoid memory leaks
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="container mx-auto">
+      <h1>Current Viewport Width: {viewportWidth}px</h1>
+      <h1>Current Viewport Height: {viewportHeight}px</h1>
+      <h1>Current Window Height: {screenHeight}px</h1>
       <h1 className="text-3xl font-bold text-center p-6">Control de Caja</h1>
       {/* <img src="https://www.readypizzacr.com/image/92689584-09.jpg" alt="Ready Pizza" className="mx-auto" width="200" /> */}
       <DataForm />
